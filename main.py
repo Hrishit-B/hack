bae58c5e417e0cf3b941bdff357bf55b02b96c3f
Pranay Obla <apranayobla@gmail.com>
2024-09-20 13:04:14 +0530

    regression class completed

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import joblib
-from turtle import pd
-from statistics import LinearRegression
-from sklearn.linear_model import Lasso
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler, PolynomialFeatures
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression, Lasso
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, mean_squared_log_error, r2_score
+import joblib
 
 class Regression:
     def __init__(self, dataset_path, target_variable, output_path):
@@ -18,19 +20,23 @@ class Regression:
                 dataset = pd.read_csv(self.dataset_path)
             except:
                 pass
+
             try:
                 dataset = pd.read_excel(self.dataset_path)
             except:
                 pass
+
             return dataset
+        
         except FileNotFoundError:
             print("File {} not found".format(self.dataset_path))
             return None
+        
         except:
             print("Some error has occured")
             return None
     
-    def linear(self):
+    def preprocessing(self):
         dataset = self.load_dataset()
         if dataset is not None:
             X = dataset.drop(self.target_variable, axis=1)
@@ -38,92 +44,87 @@ class Regression:
 
             scaler = StandardScaler()
             X_scaled = scaler.fit_transform(X)
-            
+        
             X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
-            
-            model = LinearRegression()
-            model.fit(X_train, y_train)
-            
-            joblib.dump(model, self.model_path)
-            
-            y_pred = model.predict(X_test)
-            
-            r2 = r2_score(y_test, y_pred)
-            mse = mean_squared_error(y_test, y_pred)
-            mae = mean_absolute_error(y_test, y_pred)
-            
-            print(f"R-squared: {r2:.2f}")
-            print(f"Mean Squared Error: {mse:.2f}")
-            print(f"Mean Absolute Error: {mae:.2f}")
+            return X_train, X_test, y_train, y_test
+        else:
+            return
+        
+    def save_model(self, model):
+        joblib.dump(model, self.model_path)
 
-    def polynomial(self, degree=2):
-        dataset = self.load_dataset()
-        if dataset is not None:
-            X = dataset.drop(self.target_variable, axis=1)
-            y = dataset[self.target_variable]
+    def performance(self, y_test, y_pred):
+        r2 = r2_score(y_test, y_pred)
+        mse = mean_squared_error(y_test, y_pred)
+        rmse = np.sqrt(mse)
+        mae = mean_absolute_error(y_test, y_pred)
+        msle = mean_squared_log_error(y_test, y_pred)
+        rmsle = np.sqrt(msle)
 
-            scaler = StandardScaler()
-            X_scaled = scaler.fit_transform(X)
-
-            X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
-        
-            poly_features = PolynomialFeatures(degree=degree, include_bias=False)
-            X_train_poly = poly_features.fit_transform(X_train)
-            X_test_poly = poly_features.transform(X_test)
-        
-            model = LinearRegression()     
-            model.fit(X_train_poly, y_train)
-        
-            joblib.dump(model, self.model_path)
-            joblib.dump(poly_features, self.poly_features_path)
-        
-            y_pred = model.predict(X_test_poly)
-        
-            r2 = r2_score(y_test, y_pred)
-            mse = mean_squared_error(y_test, y_pred)
-            mae = mean_absolute_error(y_test, y_pred)
-        
-            print(f"R-squared: {r2:.2f}")
-            print(f"Mean Squared Error: {mse:.2f}")
-            print(f"Mean Absolute Error: {mae:.2f}")
+    def linear_regression(self):
+        X_train, X_test, y_train, y_test = self.preprocessing(self)
             
-    def regression_model_lasso(self, alpha=1.0):
-        dataset = self.load_dataset()
-        if dataset is not None:
-            X = dataset.drop(self.target_variable, axis=1)
-            y = dataset[self.target_variable]
+        model = LinearRegression()
+        model.fit(X_train, y_train)
+            
+        y_pred = model.predict(X_test)
+        self.performance(self, y_test, y_pred)
+
+        self.save_model(self, model)
+
+    def polynomial_regression(self):
+        X_train, X_test, y_train, y_test = self.preprocessing(self)
         
-            # Split the dataset into training and testing sets
-            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        model = PolynomialFeatures(degree=3)
+        X_poly = model.fit_transform(X_train)
+        model.fit(X_poly, y_train)
+
+        y_pred = model.predict(X_test)
+        self.performance(self, y_test, y_pred)
+
+        self.save_model(self, model)
+            
+    def lasso_regression(self):
+        X_train, X_test, y_train, y_test = self.preprocessing(self)
         
-            # Scale the features
-            scaler = StandardScaler()
-            X_train_scaled = scaler.fit_transform(X_train)
-            X_test_scaled = scaler.transform(X_test)
+        model = Lasso(selection='random', random_state=42)
+        model.fit(X_train, y_train)
         
-            # Create a Lasso regression model
-            model = Lasso(alpha=alpha, random_state=42)
-        
-            # Train the model
-            model.fit(X_train_scaled, y_train)
-        
-            # Save the trained model and scaler to files
-            joblib.dump(model, self.model_path)
-            joblib.dump(scaler, self.scaler_path)
-        
-            # Predict the target values for the testing set
-            y_pred = model.predict(X_test_scaled)
-        
-            # Evaluate the model's performance
-            r2 = r2_score(y_test, y_pred)
-            mse = mean_squared_error(y_test, y_pred)
-            mae = mean_absolute_error(y_test, y_pred)
-        
-            # Print the evaluation metrics
-            print(f"R-squared: {r2:.2f}")
-            print(f"Mean Squared Error: {mse:.2f}")
-            print(f"Mean Absolute Error: {mae:.2f}")
-        
-            # Print the number of features used by the model
-            n_features_used = np.sum(model.coef_ != 0)
-            print(f"Number of features used: {n_features_used}")
+        y_pred = model.predict(X_test)
+        self.performance(self, y_test, y_pred)
+
+        self.save_model(self, model)
+
+    def decision_tree_regressor(self):
+        X_train, X_test, y_train, y_test = self.preprocessing(self)
+            
+        model = DecisionTreeRegressor(criterion="friedman_mse", random_state=42)
+        model.fit(X_train, y_train)
+            
+        y_pred = model.predict(X_test)
+        self.performance(self, y_test, y_pred)
+
+        self.save_model(self, model)
+
+    def random_forest_regressor(self):
+        X_train, X_test, y_train, y_test = self.preprocessing(self)
+            
+        model = RandomForestRegressor(criterion="friedman_mse", random_state=42)
+        model.fit(X_train, y_train)
+            
+        y_pred = model.predict(X_test)
+        self.performance(self, y_test, y_pred)
+
+        self.save_model(self, model)
+
+    def gradient_boosting_regressor(self):
+        X_train, X_test, y_train, y_test = self.preprocessing(self)
+            
+        model = GradientBoostingRegressor(loss="huber", criterion="friedman_mse", random_state=42)
+        model.fit(X_train, y_train)
+            
+        y_pred = model.predict(X_test)
+        self.performance(self, y_test, y_pred)
+
+        self.save_model(self, model)
+
